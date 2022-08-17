a2659fb917d2ecf7c544bcf03979ecc88d2bfb42
sebastian medina <sebastianlopez_21@hotmail.com>
2022-08-17 11:21:32 -0400

    commit tree 1d38851398bee8f850afe8a28185996313d6b249

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+
+
+const returnFirstTwoDrivers = function(drivers){
+    
+    const newArray = drivers.slice(0,2);
+    return newArray;
+}
+
+returnFirstTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo']);
+
+const returnLastTwoDrivers = function(drivers){
+    const newArray = drivers.slice(2, drivers.length);
+    return newArray;
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(integer){  
+    return function(fare){ 
+        return fare * integer;
+    };
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers, func){
+    return func(drivers);
+}
